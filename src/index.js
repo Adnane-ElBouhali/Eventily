034627b6a1e807c5cb4766dfc95bc2da936e2fb8
Gh1ta <salame.ghita@inemail.ine.inpt.ma>
2022-05-12 16:30:24 +0100

    j
    m

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const app = initializeApp({
+    apiKey: "AIzaSyBXZzoJSwm_WNFIuMwBfSxPAV0p-JJL3jk",
+    authDomain: "eventilyy.firebaseapp.com",
+    projectId: "eventilyy",
+    storageBucket: "eventilyy.appspot.com",
+    messagingSenderId: "409686331672",
+    appId: "1:409686331672:web:59d66d47a574aad0c98e6d",
+  });
+
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, user => {
+    if(user !=null) {
+        console.log('logged in!');
+    } else {
+        console.log('No user');
+    }
+});
