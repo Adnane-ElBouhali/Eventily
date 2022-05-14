75faf2d2ef3661ce4f38215813eaf051d80f9485
Adnane ElBouhali <elbouhali.adnane@ine.inpt.ma>
2022-05-14 16:22:47 +0100

    .

--- dist/log_in/script.js
+++ dist/log_in/script.js
@@ -1,20 +1,20 @@
 import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js';
 import { auth } from '../index.js';
 
-
-const logInBtn = document.querySelector("#C1");
+const logInBtn = document.getElementById("C1");
 
 logInBtn.addEventListener('click', (e) => {
+    console.log("hey")
     e.preventDefault();
 
     const email = document.getElementsByClassName("eds-field-styled__input")[0].value;
     const password = document.getElementsByClassName("eds-field-styled__input")[1].value;
 
     signInWithEmailAndPassword(auth, email, password)
-    .then((cred) => {
-        console.log("User logged in : ", user.cred)
-    })
-    .catch((err) => {
-        console.log(err.message)
-    })
+        .then((cred) => {
+            console.log("User logged in : ", user.cred)
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
 })
